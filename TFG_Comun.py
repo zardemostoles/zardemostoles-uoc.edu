1a8a9cb5b1410446907bce15c33686f4421a9025
zardemostoles <62905348+zardemostoles@users.noreply.github.com>
2022-12-28 11:35:49 +0100

    Add files via upload

---
+++ TFG_Comun.py
@@ -0,0 +1,6 @@
+TFG_PATH = "/content/drive/MyDrive/TFG/"
+TFG_METADATA_ORIGINAL = TFG_PATH + "train.csv"
+TFG_METADATA_ITA = TFG_PATH + "TFG_tono_piel_ITA.csv"
+TFG_METADATA = TFG_PATH + "TFG_metadatos.csv"
+TFG_PREDICCIONES = TFG_PATH + "TFG_mejor_modelo_pred.csv"
+TFG_MODEL_AWS = TFG_PATH + "TFG_mejor_modelo.tar.gz"
