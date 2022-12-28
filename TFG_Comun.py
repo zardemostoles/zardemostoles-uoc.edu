88022c5c0d5b2798393a05fe0ca68fbaeb37be97
zardemostoles <62905348+zardemostoles@users.noreply.github.com>
2022-12-28 13:35:14 +0100

    Add files via upload

--- TFG_Comun.py
+++ TFG_Comun.py
@@ -3,4 +3,10 @@ TFG_METADATA_ORIGINAL = TFG_PATH + "train.csv"
 TFG_METADATA_ITA = TFG_PATH + "TFG_tono_piel_ITA.csv"
 TFG_METADATA = TFG_PATH + "TFG_metadatos.csv"
 TFG_PREDICCIONES = TFG_PATH + "TFG_mejor_modelo_pred.csv"
+
+TFG_MODEL = TFG_PATH + "TFG_mejor_modelo.hdf5"
 TFG_MODEL_AWS = TFG_PATH + "TFG_mejor_modelo.tar.gz"
+
+TFG_AWS_CREDENTIALS = TFG_PATH + "awscli.ini"
+TFG_AWS_BUCKET = "s3://uoc-tfg/"
+TFG_MODEL_AWS_S3 = TFG_AWS_BUCKET + "TFG_mejor_modelo.tar.gz"
